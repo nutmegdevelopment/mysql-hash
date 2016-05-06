c8e28d77725bc4d80db8f3ca33b1bf09bd60a457
James Findley <jamesf@nutmeg.com>
2016-05-06 14:58:22 +0100

    Add -generate option

--- main.go
+++ main.go
@@ -1,14 +1,32 @@
 package main
 
 import (
+	"crypto/rand"
 	"crypto/sha1"
 	"encoding/hex"
+	"flag"
 	"fmt"
+	"math/big"
 	"os"
 	"strings"
 )
 
-func hash(input string) string {
+const (
+	passwordLen = 32 // Limit for replication
+)
+
+var (
+	charList = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
+	generate bool
+)
+
+func init() {
+	flag.BoolVar(&generate, "generate", false, "Generate a new random password")
+	flag.Parse()
+}
+
+// Hash generates a MySQL compathible hash
+func Hash(input string) string {
 	in := []byte(input)
 
 	// sha1(sha1(input))
@@ -21,13 +39,29 @@ func hash(input string) string {
 
 }
 
+// NewPass generates a random password
+func NewPass() string {
+	var pass [passwordLen]byte
+
+	for i := range pass {
+		val, err := rand.Int(rand.Reader, big.NewInt(int64(passwordLen)))
+		if err != nil {
+			panic(err)
+		}
+		pass[i] = charList[val.Int64()]
+	}
+	return string(pass[:])
+}
+
 func main() {
 
-	if len(os.Args) <= 1 {
-		fmt.Println("Usage: mysql-hash password")
-		os.Exit(1)
-	}
+	flag.Arg(0)
 
-	fmt.Println(hash(os.Args[1]))
+	if !generate {
+		fmt.Printf("hash:\t\t%s\n", Hash(os.Args[1]))
+	} else {
+		pass := NewPass()
+		fmt.Printf("password:\t%s\nhash:\t\t%s\n", pass, Hash(pass))
+	}
 
 }
--- main_test.go
+++ main_test.go
@@ -6,9 +6,18 @@ import (
 
 func TestHash(t *testing.T) {
 
-	output := hash("right")
+	output := Hash("right")
 	if output != "*920018161824B14A1067A69626595E68CB8284CB" {
 		t.Fatal("Incorrect hash:", output)
+	}
+
+}
+
+func TestNewPass(t *testing.T) {
+
+	pass := NewPass()
+	if len(pass) != passwordLen {
+		t.Fatal("Bad length")
 	}
 
 }
